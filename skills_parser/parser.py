eaba8232d7ed384acec6444ed551f405aca31b37
Boring-Mind <super.d3280@ya.ru>
2020-08-15 07:58:14 -0700

    feet: added top language parsing

--- skills_parser/parser.py
+++ skills_parser/parser.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from operator import itemgetter
 from typing import Dict, List
 
@@ -19,9 +20,9 @@ class Repository(object):
         self.age = age
 
 
-class UserParser(object):
+class RepoParser(object):
     username: str
-    repo_list: List[str]
+    repo_list: List[Repository]
 
     def __init__(self, username, *args, **kwargs):
         self.username = username
@@ -41,26 +42,55 @@ class UserParser(object):
         return not response.get('message', None)
 
     @classmethod
-    def sort_repos_by(cls, field: str, repos: List[dict]):
+    def sort_repos_by(cls, field: str, repos: List[dict]) -> List[dict]:
         if repos[0].get(field) is None:
             return
         return sorted(repos, key=itemgetter(field), reverse=True)
 
     @classmethod
-    def parse_repos(cls, response: Dict[str, str]) -> List[str]:
-        """Parse retrieved user repo list."""
+    def get_repos_age(cls, response: List[dict]) -> Dict[str, int]:
+        """Retrieve a list of repos age in months."""
+        result = {}
+        for repo in response:
+            begin = datetime.strptime(repo['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+            end = datetime.strptime(repo['pushed_at'], "%Y-%m-%dT%H:%M:%SZ")
+            age = (end - begin).days // 30
+            result[repo['name']] = age
+        return result
+
+    def parse_repos(self, response: Dict[str, str]) -> List[Repository]:
+        """Parse retrieved user repo list.
+
+        Returns list of Repository instances
+        initialized by names.
+        """
         raw = response.json()
         raw = __class__.sort_repos_by('size', raw)
-        
-        parsed: List[str] = []
+
+        import pdb; pdb.set_trace()
         # Select 15 biggest repos
         # Needed in order to reduce the server load
-        for repo in raw[:15]:
-            parsed.append(repo['name'])
+        ages = __class__.get_repos_age(raw[:15])
+        # Filter repositories which are at least one month old
+        ages = {r: ages[r] for r in ages.keys() if ages[r] > 0}
+
+        
+        parsed = []
+        for repo_name in ages.keys():
+            # Get top languages from the project
+            languages = self.get_top_languages(repo_name)
+            new = Repository(
+                repo_name, age=ages[repo_name], languages=languages
+            )
+            parsed.append(new)
         return parsed
 
-    def get_repos(self) -> List[str]:
-        """Retrieve and parse user repo list."""
+    def get_repos(self) -> List[Repository]:
+        """Retrieve and parse user repo list.
+
+        Returns list of processed instances
+        of Repository class.
+        """
         # Get up to 100 latest repos
         response = requests.get(
             f'https://api.github.com/users/{self.username}/repos',
@@ -68,7 +98,7 @@ class UserParser(object):
                 'sort': 'pushed',
             }
         )
-        self.repo_list = __class__.parse_repos(response)
+        self.repo_list = self.parse_repos(response)
         return self.repo_list
 
     def get_top_languages(self, repo_name: str) -> List[str]:
